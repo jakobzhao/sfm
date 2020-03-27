e549ab408b058c20d48722d452aa71eeb01b9183
Steven Bao <whichsteven297@gmail.com>
2020-03-27 13:15:51 -0700

    Add new data with projection

---
+++ pointclouds/hotel/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=17 +ellps=WGS84 +datum=WGS84 +no_defs",
+    "points": 23018188,
+    "boundingBox": {
+        "lx": 656391.393157959,
+        "ly": 983725.9792022705,
+        "lz": -3.3145294189453127,
+        "ux": 656927.3117980957,
+        "uy": 984261.8978424072,
+        "uz": 532.6041107177734
+    },
+    "tightBoundingBox": {
+        "lx": 656391.393,
+        "ly": 983725.979,
+        "lz": -3.315,
+        "ux": 656927.312,
+        "uy": 984157.174,
+        "uz": 141.996
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 4.641191482543945,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
